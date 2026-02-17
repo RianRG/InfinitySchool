1a7bd641f5be54d936cbc290bf1735438eadc5ae
RianRG <rianrgomes@gmail.com>
2026-02-17 20:39:16 -0300

    test particles

--- enemies/baseBoss/death.gd
+++ enemies/baseBoss/death.gd
@@ -7,7 +7,7 @@ func enter():
 	owner.stateMachine.travel("idleDown")
 	await get_tree().create_timer(0.5).timeout
 	owner.set_physics_process(false)
-	camera.screenShake(10, 10000)
+	camera.screenShake(10, 4)
 	await get_tree().create_timer(4).timeout
 	
 	owner.stateMachine.travel("death")
