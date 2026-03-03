0357b75e3a78a90a18b709c670a4f1065c469b63
RianRG <rianrgomes@gmail.com>
2026-03-03 20:25:01 -0300

    feat: iniciando sistema de cutscene da boss fight

--- characters/scripts/camera_2d.gd
+++ characters/scripts/camera_2d.gd
@@ -1,30 +1,33 @@
 extends Camera2D
 
 var shakeIntensity: float = 0.0
-var activeShakeTime = 0.0
-var shakeDecay = 5.0
-var shakeTime = 0.0
-var shakeTimeSpeed = 20.0
+var activeShakeTime: float = 0.0
+var totalShakeTime: float = 0.0  # <- guarda o tempo total
+var shakeTime: float = 0.0
+var shakeTimeSpeed: float = 20.0
 var noise = FastNoiseLite.new()
+
 func _physics_process(delta: float):
-	
-	if activeShakeTime>0:
-		shakeTime+=delta*shakeTimeSpeed
-		activeShakeTime-=delta
+	if activeShakeTime > 0:
+		shakeTime += delta * shakeTimeSpeed
+		activeShakeTime -= delta
+		
+		# Intensidade proporcional ao tempo restante
+		var progress = activeShakeTime / totalShakeTime
+		var currentIntensity = shakeIntensity * progress
+		
 		offset = Vector2(
-			noise.get_noise_2d(shakeTime, 0)*shakeIntensity,
-			noise.get_noise_2d(0, shakeTime)*shakeIntensity
+			noise.get_noise_2d(shakeTime, 0) * currentIntensity,
+			noise.get_noise_2d(0, shakeTime) * currentIntensity
 		)
-		
-		shakeIntensity = max(shakeIntensity - shakeDecay*delta, 0)
 	else:
-		offset = lerp(offset, Vector2.ZERO, 10.5*delta)
-		
-func screenShake(intensity: int, time: float):
+		offset = lerp(offset, Vector2.ZERO, 10.5 * delta)
+
+func screenShake(intensity: float, time: float):
 	randomize()
 	noise.seed = randi()
 	noise.frequency = 2.0
 	shakeIntensity = intensity
-	
 	activeShakeTime = time
+	totalShakeTime = time  # <- salva o total
 	shakeTime = 0.0
---
+++ scenes/scripts/juizScene.gd
@@ -0,0 +1,43 @@
+extends Node2D
+@onready var boss: CharacterBody2D = $Juiz
+@onready var camera: Camera2D = $Camera2D
+@onready var player: Player = $player
+var cutscenePlayed:=false
+@onready var playerPhantom: PhantomCamera2D = $PhantomCamera2D
+@onready var bossPhantom: PhantomCamera2D = $bossCamera
+
+func _ready() -> void:
+	print(13213123213)
+	startCutscene()
+
+func _process(delta: float) -> void:
+	pass
+	
+func startCutscene():
+	player.set_physics_process(false)
+	player.set_process_input(false)
+	boss.set_physics_process(false)
+	await get_tree().create_timer(.4).timeout
+	#playerPhantom.set_tween_ease(1)
+	#bossPhantom.set_tween_ease(1)
+	playerPhantom.set_tween_transition(5)
+	bossPhantom.set_tween_transition(5)
+	
+	playerPhantom.set_tween_duration(3)
+	bossPhantom.set_tween_duration(3)
+	
+	# 2. Transfere a câmera para a cena (ou faz ela seguir um ponto)
+	playerPhantom.priority=0
+	bossPhantom.priority=10
+	await get_tree().create_timer(2).timeout
+	camera.screenShake(7,5 )
+	await get_tree().create_timer(5).timeout
+	endCutscene()
+	# 6. Shake da câmera
+	
+func endCutscene():
+	player.set_physics_process(true)
+	player.set_process_input(true)
+	boss.set_physics_process(true)
+	playerPhantom.priority=10
+	bossPhantom.priority=0
