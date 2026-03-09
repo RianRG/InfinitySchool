86691420bfac18b56c3d6790599ffd0fc29d56a4
RianRG <rianrgomes@gmail.com>
2026-03-09 12:33:46 -0300

    martelo do juiz

---
+++ enemies/juizBoss/juiz_hammer.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@onready var toRetreatTimer: Timer = $toRetreat
+
+@onready var toAttackTimer: Timer = $toAttack
+@onready var animation: AnimationPlayer = $AnimationPlayer
+
+#func _physics_process(delta: float) -> void:
+#
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("character"):
+		body.takeDamage(global_position, 500.0, 3)
+
+
+func _on_to_attack_timeout() -> void:
+	animation.play("attack")
+	await get_tree().create_timer(3).timeout
+	animation.play("reset")
+	toAttackTimer.start()
