3c1269dab663e1bf427e1412c97cd5d2f70ce567
RianRG <rianrgomes@gmail.com>
2026-05-22 19:07:16 -0300

    vfx diagonal martelo juiz

--- enemies/Bosses/juizBoss/juiz_hammer.gd
+++ enemies/Bosses/juizBoss/juiz_hammer.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @onready var toRetreatTimer: Timer = $toRetreat
 @onready var camera: Camera2D = $"../Camera2D"
 
+@onready var vfxAnimation: AnimationPlayer = $HammerVFXAnimation
+
 @onready var toAttackTimer: Timer = $toAttack
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
@@ -18,6 +20,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 
 func _on_to_attack_timeout() -> void:
+	
+	vfxAnimation.play("X")
+	await get_tree().create_timer(.4).timeout
 	animation.play("attack")
 	await get_tree().create_timer(2).timeout
 	animation.play("reset")
@@ -26,3 +31,16 @@ func _on_to_attack_timeout() -> void:
 
 func attack():
 	camera.screenShake(10, 0.5)
+
+
+#func _on_hammer_vfx_animation_animation_finished(anim_name: StringName) -> void:
+	#if anim_name == "X":
+		#print()
+
+
+func _on_vfx_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("character"):
+		body.takeDamage(global_position, 200.0, 3)
+		
+		body.freezeFrame(0.5, .7)
+	pass # Replace with function body.
