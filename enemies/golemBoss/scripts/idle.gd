ec58c262bebe0645226562aa3b33fddc4d3e6219
Steven1-afk <marcosgabrielrochaferraz2@gmail.com>
2026-06-29 16:52:02 -0300

    tiletype test

--- enemies/golemBoss/scripts/idle.gd
+++ enemies/golemBoss/scripts/idle.gd
@@ -19,4 +19,3 @@ func enter():
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.is_in_group("character"):
 		get_parent().change_state("follow")
-		
--- prota/player.gd
+++ prota/player.gd
@@ -12,7 +12,6 @@ class_name Player
 @onready var walkstart = $WalkStart
 @onready var walkidle = $Walkidle
 
-
 # ========== 
 # LIFE HUD
 # ==========
